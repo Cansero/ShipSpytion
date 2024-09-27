1750edcdc3f3c51082d66936e5b02e2d4bf7ab1a
Cansero <cansero@V7-WIN>
2024-09-26 23:23:59 -0400

    WIP

--- Models.py
+++ Models.py
@@ -1,15 +1,15 @@
-class BaseModel:
-    def update(self, ref):
-        if not isinstance(ref, dict):
-            return
-
-        keys = vars(self).keys()
-        for k, v in ref.items():
-            if k in keys:
-                setattr(self, k, v)
-
-    def as_dict(self):
-        return vars(self)
+from SubModels import (
+        BaseModel,
+        Address,
+        AdvancedOptions,
+        CustomsItems,
+        Dimensions,
+        InsuranceOptions,
+        ProductCategory,
+        ProductTag,
+        ProductType,
+        Weight
+        )
 
 
 class Limits:
@@ -58,9 +58,9 @@ class Order(BaseModel):
             customerId=0,
             customerUsername="",
             customerEmail="",
-            billTo="",
-            shipTo="",
-            items="",
+            billTo=Address(),
+            shipTo=Address(),
+            items=None,
             orderTotal=0,
             amountPaid=0,
             taxAmount=0,
@@ -77,10 +77,10 @@ class Order(BaseModel):
             confirmation="",
             shipDate="",
             holdUntilDate="",
-            weight="",
-            dimensions="",
-            insuranceOptions="",
-            advancedOptions="",
+            weight=Weight(),
+            dimensions=Dimensions(),
+            insuranceOptions=InsuranceOptions(),
+            advancedOptions=AdvancedOptions(),
             tagIds=None,
             userId="",
             externallyFulfilled=False,
@@ -100,7 +100,7 @@ class Order(BaseModel):
         self.customerEmail = customerEmail
         self.billTo = billTo
         self.shipTo = shipTo
-        self.items = items
+        self.items = [] if items is None else items
         self.orderTotal = orderTotal
         self.amountPaid = amountPaid
         self.taxAmount = taxAmount
@@ -127,125 +127,11 @@ class Order(BaseModel):
         self.externallyFulfilledBy = externallyFulfilledBy
 
 
-class Address(BaseModel):
-    def __init__(
-            self,
-            name="",
-            company="",
-            street1="",
-            street2="",
-            street3="",
-            city="",
-            state="",
-            postalCode="",
-            country="",
-            phone="",
-            residential=False,
-            addressVerified=""
-            ):
-        self.name = name
-        self.company = company
-        self.street1 = street1
-        self.street2 = street2
-        self.street3 = street3
-        self.city = city
-        self.state = state
-        self.postalCode = postalCode
-        self.country = country
-        self.phone = phone
-        self.residential = residential
-        self.addressVerified = addressVerified
-
-
-class AdvancedOptions(BaseModel):
-    def __init__(
-            self,
-            warehouseId=0,
-            nonMachinable=False,
-            saturdayDelivery=False,
-            containsAlcohol=False,
-            storeId=0,
-            customField1="",
-            customField2="",
-            customField3="",
-            source="",
-            mergedOrSplit=False,
-            mergedIds=None,
-            parentId=0,
-            billToParty="",
-            billToAccount="",
-            billToPostalCode="",
-            billToCountryCode="",
-            billToMyOtherAccount=""
-            ):
-        self.warehouseId = warehouseId
-        self.nonMachinable = nonMachinable
-        self.saturdayDelivery = saturdayDelivery
-        self.containsAlcohol = containsAlcohol
-        self.storeId = storeId
-        self.customField1 = customField1
-        self.customField2 = customField2
-        self.customField3 = customField3
-        self.source = source
-        self.mergedOrSplit = mergedOrSplit
-        self.mergedIds = mergedIds
-        self.parentId = parentId
-        self.billToParty = billToParty
-        self.billToAccount = billToAccount
-        self.billToPostalCode = billToPostalCode
-        self.billToCountryCode = billToCountryCode
-        self.billToMyOtherAccount = billToMyOtherAccount
-
-
-class CustomsItems(BaseModel):
-    def __init__(
-            self,
-            customsItemId="",
-            description="",
-            quantity=0,
-            value=0,
-            harmonizedTariffCode="",
-            countryOfOrigin=""
-            ):
-        self.customsItemId = customsItemId
-        self.description = description
-        self.quantity = quantity
-        self.value = value
-        self.harmonizedTariffCode = harmonizedTariffCode
-        self.countryOfOrigin = countryOfOrigin
-
-
-class Dimensions(BaseModel):
-    def __init__(
-            self,
-            length=0,
-            width=0,
-            height=0,
-            units=""
-            ):
-        self.length = length
-        self.width = width
-        self.height = height
-        self.units = units
-
-
-class InsuranceOptions(BaseModel):
-    def __init__(
-            self,
-            provider="",
-            insureShipment=False,
-            insuredValue=0
-            ):
-        self.provider = provider
-        self.insureShipment = insureShipment
-        self.insuredValue = insuredValue
-
-
 class InternationalOptions(BaseModel):
     def __init__(
             self,
             contents="",
-            customsItems=None,
+            customsItems=CustomsItems(),
             nonDelivery=""
             ):
         self.contents = contents
@@ -271,7 +157,7 @@ class OrderItem(BaseModel):
             sku="",
             name="",
             imageUrl="",
-            weight=None,
+            weight=Weight(),
             quantity=0,
             unitPrice=0,
             taxAmount=0,
@@ -296,73 +182,13 @@ class OrderItem(BaseModel):
         self.taxAmount = taxAmount
         self.shippingAmount = shippingAmount
         self.warehouseLocation = warehouseLocation
-        self.options = options
+        self.options = [] if options is None else options
         self.productId = productId
         self.fulfillmentSku = fulfillmentSku
         self.adjustment = adjustment
         self.upc = upc
         self.createDate = createDate
         self.modifyDate = modifyDate
-
-
-class ProductCategory(BaseModel):
-    def __init__(
-            self,
-            categoryId=0,
-            name=""
-            ):
-        self.categoryId = categoryId
-        self.name = name
-
-
-class ProductTag(BaseModel):
-    def __init__(
-            self,
-            tagId=0,
-            name=""
-            ):
-        self.tagId = tagId
-        self.name = name
-
-
-class ProductType(BaseModel):
-    def __init__(
-            self,
-            productTypeId=0,
-            name="",
-            weightOz=0,
-            length=0,
-            width=0,
-            height=0,
-            defaultCarrierCode="",
-            defaultServiceCode="",
-            defaultPackageCode="",
-            defaultIntlCarrierCode="",
-            defaultIntlServiceCode="",
-            defaultIntlPackageCode="",
-            customsDescription="",
-            customsValue="",
-            customsTariffNo="",
-            customsCountryCode="",
-            noCustoms=False
-            ):
-        self.productTypeId = productTypeId
-        self.name = name
-        self.weightOz = weightOz
-        self.length = length
-        self.width = width
-        self.height = height
-        self.defaultCarrierCode = defaultCarrierCode
-        self.defaultServiceCode = defaultServiceCode
-        self.defaultPackageCode = defaultPackageCode
-        self.defaultIntlCarrierCode = defaultIntlCarrierCode
-        self.defaultIntlServiceCode = defaultIntlServiceCode
-        self.defaultIntlPackageCode = defaultIntlPackageCode
-        self.customsDescription = customsDescription
-        self.customsValue = customsValue
-        self.customsTariffNo = customsTariffNo
-        self.customsCountryCode = customsCountryCode
-        self.noCustoms = noCustoms
 
 
 class Product(BaseModel):
@@ -382,8 +208,8 @@ class Product(BaseModel):
             createDate="",
             modifyDate="",
             active=False,
-            productCategory=None,
-            productType=None,
+            productCategory=ProductCategory(),
+            productType=ProductType(),
             warehouseLocation="",
             defaultCarrierCode="",
             defaultServiceCode="",
@@ -398,7 +224,7 @@ class Product(BaseModel):
             customsTariffNo="",
             customsCountryCode="",
             noCustoms=False,
-            tags=None
+            tags=ProductTag()
             ):
         self.productId = productId
         self.sku = sku
@@ -434,15 +260,3 @@ class Product(BaseModel):
 
 
 # FIX: Class Webhook Omitted
-
-
-class Weight(BaseModel):
-    def __init__(
-            self,
-            value=0,
-            units="",
-            WeightUnits=0
-            ):
-        self.value = value
-        self.units = units
-        self.WeightUnits = WeightUnits
---
+++ SubModels.py
@@ -0,0 +1,198 @@
+class BaseModel:
+    def update(self, ref):
+        if not isinstance(ref, dict):
+            return
+
+        keys = vars(self).keys()
+        for k, v in ref.items():
+            if k in keys:
+                setattr(self, k, v)
+
+    def as_dict(self):
+        return vars(self)
+
+
+class Address(BaseModel):
+    def __init__(
+            self,
+            name="",
+            company="",
+            street1="",
+            street2="",
+            street3="",
+            city="",
+            state="",
+            postalCode="",
+            country="",
+            phone="",
+            residential=False,
+            addressVerified=""
+            ):
+        self.name = name
+        self.company = company
+        self.street1 = street1
+        self.street2 = street2
+        self.street3 = street3
+        self.city = city
+        self.state = state
+        self.postalCode = postalCode
+        self.country = country
+        self.phone = phone
+        self.residential = residential
+        self.addressVerified = addressVerified
+
+
+class AdvancedOptions(BaseModel):
+    def __init__(
+            self,
+            warehouseId=0,
+            nonMachinable=False,
+            saturdayDelivery=False,
+            containsAlcohol=False,
+            storeId=0,
+            customField1="",
+            customField2="",
+            customField3="",
+            source="",
+            mergedOrSplit=False,
+            mergedIds=None,
+            parentId=0,
+            billToParty="",
+            billToAccount="",
+            billToPostalCode="",
+            billToCountryCode="",
+            billToMyOtherAccount=""
+            ):
+        self.warehouseId = warehouseId
+        self.nonMachinable = nonMachinable
+        self.saturdayDelivery = saturdayDelivery
+        self.containsAlcohol = containsAlcohol
+        self.storeId = storeId
+        self.customField1 = customField1
+        self.customField2 = customField2
+        self.customField3 = customField3
+        self.source = source
+        self.mergedOrSplit = mergedOrSplit
+        self.mergedIds = [] if mergedIds is None else mergedIds
+        self.parentId = parentId
+        self.billToParty = billToParty
+        self.billToAccount = billToAccount
+        self.billToPostalCode = billToPostalCode
+        self.billToCountryCode = billToCountryCode
+        self.billToMyOtherAccount = billToMyOtherAccount
+
+
+class CustomsItems(BaseModel):
+    def __init__(
+            self,
+            customsItemId="",
+            description="",
+            quantity=0,
+            value=0,
+            harmonizedTariffCode="",
+            countryOfOrigin=""
+            ):
+        self.customsItemId = customsItemId
+        self.description = description
+        self.quantity = quantity
+        self.value = value
+        self.harmonizedTariffCode = harmonizedTariffCode
+        self.countryOfOrigin = countryOfOrigin
+
+
+class Dimensions(BaseModel):
+    def __init__(
+            self,
+            length=0,
+            width=0,
+            height=0,
+            units=""
+            ):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.units = units
+
+
+class InsuranceOptions(BaseModel):
+    def __init__(
+            self,
+            provider="",
+            insureShipment=False,
+            insuredValue=0
+            ):
+        self.provider = provider
+        self.insureShipment = insureShipment
+        self.insuredValue = insuredValue
+
+
+class ProductCategory(BaseModel):
+    def __init__(
+            self,
+            categoryId=0,
+            name=""
+            ):
+        self.categoryId = categoryId
+        self.name = name
+
+
+class ProductTag(BaseModel):
+    def __init__(
+            self,
+            tagId=0,
+            name=""
+            ):
+        self.tagId = tagId
+        self.name = name
+
+
+class ProductType(BaseModel):
+    def __init__(
+            self,
+            productTypeId=0,
+            name="",
+            weightOz=0,
+            length=0,
+            width=0,
+            height=0,
+            defaultCarrierCode="",
+            defaultServiceCode="",
+            defaultPackageCode="",
+            defaultIntlCarrierCode="",
+            defaultIntlServiceCode="",
+            defaultIntlPackageCode="",
+            customsDescription="",
+            customsValue="",
+            customsTariffNo="",
+            customsCountryCode="",
+            noCustoms=False
+            ):
+        self.productTypeId = productTypeId
+        self.name = name
+        self.weightOz = weightOz
+        self.length = length
+        self.width = width
+        self.height = height
+        self.defaultCarrierCode = defaultCarrierCode
+        self.defaultServiceCode = defaultServiceCode
+        self.defaultPackageCode = defaultPackageCode
+        self.defaultIntlCarrierCode = defaultIntlCarrierCode
+        self.defaultIntlServiceCode = defaultIntlServiceCode
+        self.defaultIntlPackageCode = defaultIntlPackageCode
+        self.customsDescription = customsDescription
+        self.customsValue = customsValue
+        self.customsTariffNo = customsTariffNo
+        self.customsCountryCode = customsCountryCode
+        self.noCustoms = noCustoms
+
+
+class Weight(BaseModel):
+    def __init__(
+            self,
+            value=0,
+            units="",
+            WeightUnits=0
+            ):
+        self.value = value
+        self.units = units
+        self.WeightUnits = WeightUnits
